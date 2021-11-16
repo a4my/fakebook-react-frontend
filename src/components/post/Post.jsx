f249ca12acfce538b7269f23486415595459bdd0
Alex Fourmy <alexfourmy@gmail.com>
2021-11-16 19:53:06 +0100

    Made the post section dynamic

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,34 +1,57 @@
 import './post.css'
 import { MoreVert } from '@material-ui/icons'
+import { Users } from '../../dummyData'
+import { useState } from 'react'
 
-export default function Post() {
+export default function Post({ post }) {
+  const [like, setLike] = useState(post.like)
+  const [isLiked, setIsLiked] = useState(false)
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1)
+    setIsLiked(!isLiked)
+  }
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img className="postProfileImg" src="/assets/person/1.jpg" alt="" />
-            <span className="postUsername">Elon Musk</span>
-            <span className="postDate">5 mins ago</span>
+            <img
+              className="postProfileImg"
+              src={Users.filter(u => u.id === post.userId)[0].profilePicture}
+              alt=""
+            />
+            <span className="postUsername">
+              {Users.filter(u => u.id === post.userId)[0].username}
+            </span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">
-            Hey! It's my first post, to the Moon!!
-          </span>
-          <img className="postImg" src="assets/post/1.jpg" alt="" />
+          <span className="postText">{post?.desc}</span>
+          <img className="postImg" src={post.photo} alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="assets/like.png" alt="" />
-            <img className="likeIcon" src="assets/heart.png" alt="" />
-            <span className="postLikeCounter">32M people like this</span>
+            <img
+              className="likeIcon"
+              src="assets/like.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              src="assets/heart.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <span className="postLikeCounter">{like}</span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">9K comments</span>
+            <span className="postCommentText">{post.comment}</span>
           </div>
         </div>
       </div>
