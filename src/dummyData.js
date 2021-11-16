d0f27810e8ae0dbac2d86b612ea4977d1c4e8962
Alex Fourmy <alexfourmy@gmail.com>
2021-11-16 19:04:08 +0100

    This file contains dummy data for the users and posts

---
+++ src/dummyData.js
@@ -0,0 +1,156 @@
+export const Users = [
+  {
+    id: 1,
+    profilePicture: 'assets/person/1.jpg',
+    username: 'Elon Musk'
+  },
+  {
+    id: 2,
+    profilePicture: 'assets/person/2.jpg',
+    username: 'Thomas Pesquet'
+  },
+  {
+    id: 3,
+    profilePicture: 'assets/person/3.jpg',
+    username: 'Paul Watson'
+  },
+  {
+    id: 4,
+    profilePicture: 'assets/person/4.jpg',
+    username: 'Bill Gates'
+  },
+  {
+    id: 5,
+    profilePicture: 'assets/person/5.jpg',
+    username: 'Greta Thunberg'
+  },
+  {
+    id: 6,
+    profilePicture: 'assets/person/6.jpg',
+    username: 'Sid Meier'
+  },
+  {
+    id: 7,
+    profilePicture: 'assets/person/7.jpg',
+    username: 'David Attenborough'
+  },
+  {
+    id: 8,
+    profilePicture: 'assets/person/8.jpg',
+    username: 'Barack Obama'
+  },
+  {
+    id: 9,
+    profilePicture: 'assets/person/9.jpg',
+    username: 'Leonardo Dicaprio'
+  },
+  {
+    id: 10,
+    profilePicture: 'assets/person/10.jpg',
+    username: 'Luc Besson'
+  },
+  {
+    id: 11,
+    profilePicture: 'assets/person/11.jpg',
+    username: 'Bernard Werber'
+  },
+  {
+    id: 12,
+    profilePicture: 'assets/person/12.jpg',
+    username: 'Lady Gaga'
+  },
+  {
+    id: 13,
+    profilePicture: 'assets/person/13.jpg',
+    username: 'Michael Jordan'
+  }
+]
+
+export const Posts = [
+  {
+    id: 1,
+    desc: 'Thank you Crew-4 for flying with SpaceX, welcome back to planet Earth!',
+    photo: 'assets/post/1.jpg',
+    date: '5 mins ago',
+    userId: 1,
+    like: 32,
+    comment: '9K comments'
+  },
+  {
+    id: 2,
+    photo: 'assets/post/2.jpeg',
+    date: '15 mins ago',
+    userId: 9,
+    like: 2,
+    comment: '78K comments'
+  },
+  {
+    id: 3,
+    desc: 'Another future is possible.',
+    photo: 'assets/post/3.jpg',
+    date: '1 hour ago',
+    userId: 5,
+    like: 61,
+    comment: '7K comments'
+  },
+  {
+    id: 4,
+    desc: 'It looked worse than last time I was on the ISS...',
+    photo: 'assets/post/4.jpg',
+    date: '4 hours ago',
+    userId: 2,
+    like: 7,
+    comment: '54K comments'
+  },
+  {
+    id: 5,
+    photo: 'assets/post/5.jpg',
+    date: '5 hours ago',
+    userId: 11,
+    like: 23,
+    comment: '1K comments'
+  },
+  {
+    id: 6,
+    photo: 'assets/post/6.jpg',
+    date: '1 day ago',
+    userId: 6,
+    like: 44,
+    comment: '2K comments'
+  },
+  {
+    id: 7,
+    desc: 'We will never stop fighting!',
+    photo: 'assets/post/7.png',
+    date: '2 days ago',
+    userId: 3,
+    like: 52,
+    comment: '34K comments'
+  },
+  {
+    id: 8,
+    desc: '#tbt',
+    photo: 'assets/post/8.jpg',
+    date: '3 days ago',
+    userId: 10,
+    like: 15,
+    comment: '11K comments'
+  },
+  {
+    id: 9,
+    desc: "Berlin's gigafactory is almost complete.",
+    photo: 'assets/post/9.jpg',
+    date: '5 days ago',
+    userId: 1,
+    like: 11,
+    comment: '41K comments'
+  },
+  {
+    id: 10,
+    photo: 'assets/post/10.jpg',
+    date: '1 week ago',
+    userId: 13,
+    like: 104,
+    comment: '57K comments'
+  }
+]
